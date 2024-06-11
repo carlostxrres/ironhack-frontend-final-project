ac3b236dff8adea50d92d951cc819ff0465e0dd9
Carlos Torres <ctorresmoral@gmail.com>
2024-06-11 21:29:42 +0200

    add icons

---
+++ src/components/icons/IconBaseline.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { defineProps } from 'vue'
+const { size, strokeWidth } = defineProps(['size', 'strokeWidth'])
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="size || 24"
+    :height="size || 24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    :stroke-width="strokeWidth || 2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M4 3h16" />
+    <path d="M4 9h16" />
+    <path d="M4 15h16" />
+    <path d="M4 21h16" />
+  </svg>
+</template>
---
+++ src/components/icons/IconPlus.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { defineProps } from 'vue'
+const { size, strokeWidth } = defineProps(['size', 'strokeWidth'])
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="size || 24"
+    :height="size || 24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    :stroke-width="strokeWidth || 2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M12 5l0 14" />
+    <path d="M5 12l14 0" />
+  </svg>
+</template>
