46411aea209ce51bf9978754c4780c92225823bd
Torres Moral <ntorcarl@amazon.es>
2024-06-24 10:04:20 +0200

    componetize createTaskButton

---
+++ src/components/CreateTaskButton.vue
@@ -0,0 +1,25 @@
+<script setup>
+import IconPlus from "@/components/icons/IconPlus.vue";
+import { ref, computed } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const isCreatingTask = computed(() => route.path.includes("create-task"));
+</script>
+<template>
+  <RouterLink
+    to="./tasks/create-task"
+    class="discreet-anchor"
+    :class="{ disabled: isCreatingTask }"
+  >
+    <label class="button-icon-wrap new-task-button">
+      <button class="button-icon button-primary">
+        <IconPlus strokeWidth="4" />
+      </button>
+      <span>Add new task</span>
+    </label>
+  </RouterLink>
+</template>
+
+<style>
+</style>
--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -5,59 +5,46 @@
 import PageHeader from "@/components/PageHeader.vue";
 import SimpleLayout from "@/components/SimpleLayout.vue";
 import TaskComponent from "@/components/TaskComponent.vue";
-import IconPlus from "@/components/icons/IconPlus.vue";
 import IconRefresh from "@/components/icons/IconRefresh.vue";
 import IconSortAscending from "@/components/icons/IconSortAscending.vue";
 import TaskList from "@/components/TaskList.vue";
+import CreateTaskButton from "@/components/CreateTaskButton.vue";
 
 import { ref } from "vue";
 import { RouterView } from "vue-router";
 import { useRouter } from "vue-router";
-import { useTaskStore } from "@/stores/task.js";
-
-const taskStore = useTaskStore();
 
 const router = useRouter();
-
-taskStore.fetchTasks();
-const taskCounts = ref({
-  total: taskStore.tasks,
-  completed: taskStore.tasks.filter((task) => task.is_complete),
-  incomplete: taskStore.tasks.filter((task) => !task.is_complete),
-});
-
 // const userStore = useUserStore()
 // const toasterStore = useToasterStore()
 const navigateToCreateTask = () => {
   router.push("create-task");
 };
+
+// to do: if no user in localStorage, just go redirect to sign-in view
 </script>
+
 <template>
   <SimpleLayout>
     <template v-slot:header>
       <h1>Your tasks</h1>
+      <!-- to do: put the nav bar here in the header so it's sticky -->
     </template>
     <template v-slot:content>
-      <!-- Tasks List -->
-      <p v-if="!taskStore.tasks">You have no tasks yet.</p>
-      <div v-else>
-        <TaskList />
-      </div>
-      <!-- Tasks List: end -->
-
-      <!-- Create New Task -->
-      <label class="button-icon-wrap">
-        <button class="button-icon button-primary" @click="navigateToCreateTask">
-          <IconPlus strokeWidth="4" />
-        </button>
-        <span>Add new task</span>
-      </label>
-      <!-- Create New Task: end -->
+      <TaskList />
+    </template>
+    <template v-slot:floating>
+      <CreateTaskButton />
+      <RouterView />
     </template>
   </SimpleLayout>
 </template>
 
 <style>
+.new-task-button {
+  margin-block: 1rem;
+}
+
 .button-icon-wrap {
   display: flex;
   align-items: center;
