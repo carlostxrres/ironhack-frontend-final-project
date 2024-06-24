5d792fa831a9f976a157abe575ae2247877e883b
Torres Moral <ntorcarl@amazon.es>
2024-06-24 10:07:46 +0200

    make footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,12 +1,26 @@
 <script setup>
+import { removeUser, isLoggedIn } from "@/services/session.js";
+import router from "@/router/index.js";
+const logOut = () => {
+  removeUser();
+  router.push("/sign-in");
+};
 </script>
 
 <template>
-  <footer class="page-footer">This is the footer</footer>
+  <footer class="page-footer">
+    <p>This is the footer</p>
+    <button @click="logOut" class="button-tertiary" v-if="isLoggedIn()">Log out</button>
+  </footer>
 </template>
 
 <style>
-/* .page-footer {
-    margin-top: auto !important;
-} */
+.page-footer {
+  align-self: normal;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-top: 1px solid #ccc;
+}
 </style>
