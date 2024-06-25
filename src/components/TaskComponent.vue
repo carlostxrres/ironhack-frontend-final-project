d50eb2ab5fc28d401f76aa25e86718821b2b68bb
Carlos Torres <ctorresmoral@gmail.com>
2024-06-25 21:03:58 +0200

    fix link to Task Detail view

--- src/components/TaskComponent.vue
+++ src/components/TaskComponent.vue
@@ -106,7 +106,7 @@ const deleteTask = async () => {
       <button class="button-icon button-tertiary" @click="deleteTask">
         <IconTrash size="18" />
       </button>
-      <RouterLink :to="task.id">
+      <RouterLink :to="'/task/' + task.id">
         <IconArrowRight size="18" />
       </RouterLink>
     </div>
