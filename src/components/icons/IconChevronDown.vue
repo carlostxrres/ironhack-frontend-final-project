685698fa3d9f45b460436d32ec56bfbe60d58c90
Torres Moral <ntorcarl@amazon.es>
2024-07-01 12:15:25 +0200

    add icon Chevron Down

---
+++ src/components/icons/IconChevronDown.vue
@@ -0,0 +1,20 @@
+<script setup>
+const { size, strokeWidth } = defineProps(["size", "strokeWidth"]);
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="size || 24"
+    :height="size || 24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    :stroke-width="strokeWidth || 2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M6 9l6 6l6 -6" />
+  </svg>
+</template>
