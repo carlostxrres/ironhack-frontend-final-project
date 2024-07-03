0a2d4341a83208ed8792055107db550fe1580fa6
Carlos Torres <ctorresmoral@gmail.com>
2024-07-03 21:52:58 +0200

    add logo to header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,4 +1,5 @@
 <script setup>
+import IconSword from '@/components/icons/IconSword.vue'
 const date = new Date().toLocaleDateString('en-US', {
   month: 'long',
   day: 'numeric',
@@ -8,7 +9,10 @@ const date = new Date().toLocaleDateString('en-US', {
 
 <template>
   <header class="page-header">
-    <h2>To do app</h2>
+    <span class="icon-entry">
+      <IconSword size="28" style="color: var(--primary-color-1)" />
+      <h2>To do app</h2>
+    </span>
     <p>{{ date }}</p>
   </header>
 </template>
---
+++ src/components/icons/IconSword.vue
@@ -0,0 +1,21 @@
+<script setup>
+const { size, strokeWidth } = defineProps(['size', 'strokeWidth'])
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="size || 24"
+    :height="size || 24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    :stroke-width="strokeWidth || 2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M20 4v5l-9 7l-4 4l-3 -3l4 -4l7 -9z" />
+    <path d="M6.5 11.5l6 6" />
+  </svg>
+</template>
